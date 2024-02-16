719f125c062ccfa1da8674050dc5eaa6d16d444a
Philip Klare <philipklare@gmail.com>
2024-02-16 11:22:19 +0100

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using System.Windows;
+
+
+namespace WPF_JsonCreator_2024_02_06;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        DatePckr.SelectedDate = new DateTime(1900, 1, 1);
+        
+    }
+
+    public List<Person> LPerson = new List<Person>();
+    public void BtnCreateJson_Click(object sender, RoutedEventArgs e)
+    {
+        Person person = new Person();
+        person.Name = NameTxt.Text;
+        person.Vorname = VornameTxt.Text;
+        person.Geburtsdatum = DatePckr.SelectedDate.Value;
+        LPerson.Add(person);
+        string json = JsonSerializer.Serialize(LPerson, new JsonSerializerOptions { WriteIndented = true });
+        JsonTxt.Text = json;
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,9 @@
+﻿namespace WPF_JsonCreator_2024_02_06;
+
+public class Person
+{
+    public string Name { get; set; }
+    public string Vorname { get; set; }
+    public DateTime Geburtsdatum { get; set; }
+    
+}
